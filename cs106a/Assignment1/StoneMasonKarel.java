102ebeda3b684bdaf0b9c6a0bcab0523d9bae280
slam-shark <dmytromanuk@gmail.com>
2014-06-09 22:33:12 +0300

    Ass1 - stones

--- cs106a/Assignment1/StoneMasonKarel.java
+++ cs106a/Assignment1/StoneMasonKarel.java
@@ -12,6 +12,76 @@ import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
 
-	// You fill in this part
+    private static final int NUM_OF_STEPS = 4;
+
+    private enum Result {
+	OK, END;
+
+    }
+
+    public static void main(String[] args) {
+	stanford.karel.Karel.main(new String[] { "code=" + StoneMasonKarel.class.getCanonicalName() });
+    }
+
+    @Override
+    public void run() {
+	repairWorld();
+    }
+
+    private void repairWorld() {
+	repairToLeft();
+	turnRight();
+
+	Result flag = Result.OK;
+	while (flag == Result.OK) {
+
+	    if ((flag = move4times()) == Result.OK) {
+		repairToRight();
+		turnLeft();
+		if ((flag = move4times()) == Result.OK) {
+		    repairToLeft();
+		    turnRight();
+		}
+	    }
+	}
+
+    }
+
+    private void repairToLeft() {
+	turnLeft();
+	repairColumn();
+    }
+
+    private void repairToRight() {
+	turnRight();
+	repairColumn();
+
+    }
+
+    private Result move4times() {
+	for (int i = 0; i < NUM_OF_STEPS; i++) {
+	    if (frontIsClear()) {
+		move();
+	    } else {
+		return Result.END;
+	    }
+	}
+	return Result.OK;
+    }
+
+    private void repairColumn() {
+	putBeeperIfNotPresent();
+	while (frontIsClear()) {
+	    move();
+	    putBeeperIfNotPresent();
+	}
+    }
+
+    private void putBeeperIfNotPresent() {
+	if (!beepersPresent()) {
+	    putBeeper();
+	}
+
+    }
 
 }
